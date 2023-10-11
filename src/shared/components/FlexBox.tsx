88ca8312fb2057c4efa78c98519d829aad6de81a
Evan Shriner <evan.shriner@gmail.com>
2023-10-11 19:02:45 -0400

    add loco, change colors, add folor structure

---
+++ src/shared/components/FlexBox.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+type FlexboxProps = {
+  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  justifyContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch';
+  flexWrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+};
+
+const Flexbox = styled.div<FlexboxProps>`
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection || 'row'};
+  justify-content: ${(props) => props.justifyContent || 'flex-start'};
+  align-items: ${(props) => props.alignItems || 'stretch'};
+  flex-wrap: ${(props) => props.flexWrap || 'nowrap'};
+`;
+
+export default Flexbox;
