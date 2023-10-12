7a75c0d2cacdfa064435fccce048e6ffce79addb
Evan Shriner <evan.shriner@gmail.com>
2023-10-11 23:17:20 -0400

    create theme

---
+++ src/theme/emotion.d.ts
@@ -0,0 +1,8 @@
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: {
+      primaryText: string;
+      primaryBackground: string;
+    };
+  }
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,8 @@
+import { Theme } from '@emotion/react';
+
+export const theme: Theme = {
+  colors: {
+    primaryText: '#f1ede8',
+    primaryBackground: '#82887c',
+  },
+};
