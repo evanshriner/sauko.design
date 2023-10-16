56df89e0318acb491a010258b2504ff695bc8e72
Evan Shriner <evan.shriner@gmail.com>
2023-10-16 18:47:00 -0400

    change background color

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,6 +3,6 @@ import { Theme } from '@emotion/react';
 export const theme: Theme = {
   colors: {
     primaryText: '#f1ede8',
-    primaryBackground: '#82887c',
+    primaryBackground: '#c9c8c9',
   },
 };
