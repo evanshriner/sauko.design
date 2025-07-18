69653cde9bef47a560b0e0d20314c8ee0adf8b20
Evan Shriner <evan.shriner@gmail.com>
2025-07-18 12:43:15 -0400

    update menu toggle to be audio icon

--- src/shared/components/navbar/MenuToggle.tsx
+++ src/shared/components/navbar/MenuToggle.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import FlexBox from '../../../shared/components/FlexBox';
+import { LuAudioLines } from "react-icons/lu";
 
 interface MenuToggleProps {
   onClick: () => void;
@@ -13,18 +14,41 @@ const MenuToggleContainer = styled(FlexBox)({
   },
 });
 
-const MenuToggleButton = styled('button')({
-  display: 'block',
-  alignItems: 'center',
+const MenuToggleButton = styled('button')(({ theme }) => ({
   background: 'none',
   border: 'none',
+  color: theme.colors.defaultText,
+  filter: theme.colors.defaultTextFilter,
   fontSize: '2rem',
-  color: 'white',
-});
+  cursor: 'pointer',
+  padding: '8px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  transition: 'transform 0.5s ease',
+  transform: 'scale(1)',
+
+  '@keyframes pulse': {
+    '0%': {
+      opacity: 0.73,
+    },
+    '50%': {
+      opacity: 1,
+    },
+    '100%': {
+      opacity: 0.73,
+    },
+  },
+
+  '&:hover': {
+    transform: 'scale(1.2)',
+    animation: 'pulse 1.5s infinite alternate',
+  },
+}));
 
 const MenuToggle = ({ onClick }: MenuToggleProps) => (
   <MenuToggleContainer>
-    <MenuToggleButton onClick={onClick}>☰</MenuToggleButton>
+    <MenuToggleButton onClick={onClick}><LuAudioLines/></MenuToggleButton>
   </MenuToggleContainer>
 );
 
