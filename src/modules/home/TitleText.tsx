8f24217d80c642cc04e4833a4e041a0dd6c11260
Evan Shriner <eshriner@Evans-MacBook-Pro.local>
2025-07-22 11:35:33 -0400

    add property to disable selection on neon text

---
+++ src/modules/home/TitleText.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import NeonText from '@/shared/styles/NeonText';
+
+const TitleText = styled(NeonText)({
+  fontSize: '30px',
+  height: '30%',
+  justifyContent: 'center',
+  alignItems: 'flex-end',
+});
+
+export default TitleText;
--- src/shared/styles/NeonText.tsx
+++ src/shared/styles/NeonText.tsx
@@ -5,22 +5,17 @@ export interface NeonTextProps {
   fontSize?: string;
   darken?: boolean;
   animatedHover?: boolean;
+  disableSelection?: boolean;
 }
-
-const calculateTextShadow = (fontSize: string) => {
-  const size = parseFloat(fontSize) || 2; // Default to 2rem if fontSize is not provided
-  const unit = fontSize.replace(/[0-9.]/g, '') || 'rem'; // Extract unit or default to 'rem'
-  const shadowSize = size * 0.05; // Adjust multiplier as needed for desired effect
-
-  return `
-    ${shadowSize * 0.5}${unit} ${shadowSize * 0.5}${unit} var(--c),
-    ${shadowSize}${unit} ${shadowSize * 0.5}${unit} var(--c),
-  `;
-};
 
 const NeonText = styled(FlexBox)<NeonTextProps>(
   // TODO: darken should be converted to 'selected' prop
-  ({ fontSize = '2rem', darken = false, animatedHover = false, theme }) => ({
+  ({
+    fontSize = '2rem',
+    darken = false,
+    animatedHover = false,
+    disableSelection = false,
+  }) => ({
     color: `rgba(255, 255, 255, ${darken ? 0.5 : 0.73})`,
     fontSize,
     filter: 'url(#neonGlow)',
@@ -31,6 +26,12 @@ const NeonText = styled(FlexBox)<NeonTextProps>(
         '100%': { transform: 'translateY(0)' },
       },
       animation: 'hoverX 6s ease-in-out infinite',
+    }),
+    ...(disableSelection && {
+      '-webkit-user-select': 'none',
+      '-moz-user-select': 'none',
+      '-ms-user-select': 'none',
+      'user-select': 'none',
     }),
   }),
 );
